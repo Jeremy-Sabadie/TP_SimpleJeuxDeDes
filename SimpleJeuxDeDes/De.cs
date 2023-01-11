3d0d9eb12018190f780763211b1e1a8931e86408
cda07saba <jeremy.sabadie@2isa.org>
2023-01-11 12:01:11 +0100

    Ajoutez des fichiers projet.

---
+++ SimpleJeuxDeDes/De.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleJeuxDeDes
+{
+    internal class De
+    {
+        private int Value;
+        /// <summary>
+        /// Fonction GetValeue
+        /// </summary>
+        public int GetValue()
+        {
+            return this.Value;
+        }
+
+        public int Lancer()
+        {
+            Random rand = new Random();
+            Value = rand.Next(0, 7);
+            return Value;
+
+        }
+    }
+}
---
+++ SimpleJeuxDeDes/Program.cs
@@ -0,0 +1,17 @@
+﻿
+//1er De
+using SimpleJeuxDeDes;
+
+De De1 = new De();
+
+
+//2ème De
+De De2 = new De();
+int ScoreDe1 = De1.Lancer();
+int ScoreDe2 = De2.Lancer();
+Console.WriteLine($"dès n°1: {ScoreDe1}");
+while (De1.GetValue() < 6 && De2.GetValue() < 6)
+{
+    De1.Lancer();
+    De2.Lancer();
+}
